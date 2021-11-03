7158d1a944d59899cd2169762edcb413a9d96bd8
sandrbuz <sandrbuz@mail.ru>
2021-11-03 13:47:42 +0300

    sorted by the number of pages in the book

--- main.js
+++ main.js
@@ -22,21 +22,22 @@ function bookSearch() {
             const select = document.querySelector('.sort-select');
             select.addEventListener('change', function () {
                 if (select.value == 'pageCount') {
-                    const arr1 = data.items;
+                    const arrItems = data.items;
                     // function sortByFeedbacks(arr) {
                     // const temp = JSON.parse(JSON.stringify(arr));
                     // const temp = arr.slice();
                     // temp[1].id = '01';
                     // console.log(data.items[1]);
-                    arr1.sort((a, b) => a.volumeInfo.pageCount - b.volumeInfo.pageCount);
-                    for (i = 0; i < data.items.length; i++) {
-                        const urlImg = data.items[i].volumeInfo.imageLinks.thumbnail;
-                        const urlMore = data.items[i].volumeInfo.previewLink;
-                        results.innerHTML += "<div class='item'>" + "<h2 class='title'>" + data.items[i].volumeInfo.title + "</h2>" + "<h3 class='bookAuth'>" + `<img class='bookImg' src = ${urlImg}>` + `<a  target="_blank" class='linkBtn' href=${urlMore}>` + 'Read more' + "</a>" + "</div>";
+                    arrItems.sort((a, b) => a.volumeInfo.pageCount - b.volumeInfo.pageCount);
+                    document.getElementById('results').innerHTML = "";
+                    for (i = 0; i < arrItems.length; i++) {
+                        const urlImg = arrItems[i].volumeInfo.imageLinks.thumbnail;
+                        const urlMore = arrItems[i].volumeInfo.previewLink;
+                        results.innerHTML += "<div class='item'>" + "<h2 class='title'>" + arrItems[i].volumeInfo.title + "</h2>" + "<h3 class='bookAuth'>" + `<img class='bookImg' src = ${urlImg}>` + `<a  target="_blank" class='linkBtn' href=${urlMore}>` + 'Read more' + "</a>" + "</div>";
 
                     }
 
-                    // console.log(arr1);
+                    console.log(arrItems);
                     // }
                     // sortByFeedbacks(arr1);
 
