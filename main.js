7f084cb422ce16fb1ef276d50d31fd213908e110
sandrbuz <sandrbuz@mail.ru>
2021-11-18 09:41:28 +0300

    pagination attempts

--- main.js
+++ main.js
@@ -15,6 +15,17 @@ function bookSearch() {
                 results.innerHTML += "<div class='item'>" + "<h2 class='title'>" + data.items[i].volumeInfo.title + "</h2>" + "<h3 class='bookAuth'>" + `<img class='bookImg' src = ${urlImg}>` + `<a  target="_blank" class='linkBtn' href=${urlMore}>` + 'Read more' + "</a>" + "</div>";
 
             }
+            let wrap = document.querySelector('.btns-pages')
+            for (k = 10; k < data.totalItems; k + 10) {
+                let Pg = document.createElement('button');
+                Pg.style.color = 'blue';
+                Pg.style.width = '100px';
+                Pg.style.height = '100px';
+                Pg.innerHTML = 'wtf';
+                wrap.innerHTML = Pg;
+
+
+            }
 
         },
         type: 'GET'
@@ -33,30 +44,30 @@ search.onkeypress = function (event) {
 
 
 // pagination-------------------------------------------------------------------------------
-let btnPage2 = document.querySelector('.btn-page-2');
+// let btnPage2 = document.querySelector('.btn-page-2');
 // let btnPage3 = document.querySelector('.btn-page-3');
-function page2() {
-    let search = document.getElementById('search').value;
-    document.getElementById('results').innerHTML = "";
-    $.ajax({
-        url: "https://www.googleapis.com/books/v1/volumes?q=" + search + '&maxResults=10&startIndex=10',
-        dataType: "json",
+// function page2() {
+//     let search = document.getElementById('search').value;
+//     document.getElementById('results').innerHTML = "";
+//     $.ajax({
+//         url: "https://www.googleapis.com/books/v1/volumes?q=" + search + '&maxResults=10&startIndex=10',
+//         dataType: "json",
 
 
-        success: function (data) {
-            console.log(data)
+//         success: function (data) {
+//             console.log(data)
 
-            for (i = 0; i < data.items.length; i++) {
-                const urlImg = data.items[i].volumeInfo.imageLinks.thumbnail;
-                const urlMore = data.items[i].volumeInfo.previewLink;
-                results.innerHTML += "<div class='item'>" + "<h2 class='title'>" + data.items[i].volumeInfo.title + "</h2>" + "<h3 class='bookAuth'>" + `<img class='bookImg' src = ${urlImg}>` + `<a  target="_blank" class='linkBtn' href=${urlMore}>` + 'Read more' + "</a>" + "</div>";
+//             for (i = 0; i < data.items.length; i++) {
+//                 const urlImg = data.items[i].volumeInfo.imageLinks.thumbnail;
+//                 const urlMore = data.items[i].volumeInfo.previewLink;
+//                 results.innerHTML += "<div class='item'>" + "<h2 class='title'>" + data.items[i].volumeInfo.title + "</h2>" + "<h3 class='bookAuth'>" + `<img class='bookImg' src = ${urlImg}>` + `<a  target="_blank" class='linkBtn' href=${urlMore}>` + 'Read more' + "</a>" + "</div>";
 
-            }
+//             }
 
-        },
-        type: 'GET'
-    });
-}
+//         },
+//         type: 'GET'
+//     });
+// }
 // function page3() {
 //     let search = document.getElementById('search').value;
 //     document.getElementById('results').innerHTML = "";
@@ -79,7 +90,7 @@ function page2() {
 //         type: 'GET'
 //     });
 // }
-btnPage2.addEventListener('click', page2, false);
+// btnPage2.addEventListener('click', page2, false);
 // btnPage3.addEventListener('click', page3, false);
 
 // pagination-------------------------------------------------------------------------------
